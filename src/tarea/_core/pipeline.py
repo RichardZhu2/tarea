11bee03b0925a44a77044a986ead3ece0d6ad147
Richard Zhu <richard.zhu2@gmail.com>
2024-11-16 14:52:48 +0000

    add support for callable objects as pipelines

--- src/tarea/_core/pipeline.py
+++ src/tarea/_core/pipeline.py
@@ -77,7 +77,10 @@ class Task:
         if throttle < 0:
             raise ValueError("throttle cannot be less than 0")
         
-        is_gen = inspect.isgeneratorfunction(func) or inspect.isasyncgenfunction(func)
+        is_gen = inspect.isgeneratorfunction(func) \
+            or inspect.isasyncgenfunction(func) \
+            or inspect.isgeneratorfunction(func.__call__) \
+            or inspect.isasyncgenfunction(func.__call__)
         if branch and not is_gen:
             raise TypeError("A branching task must exhibit generator behaviour (use the yield keyword)")
         if not branch and is_gen:
