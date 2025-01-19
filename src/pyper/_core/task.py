6f52972d82ecd8c71208a8b2af0dd5bd51e4ccee
Richard Zhu <richard.zhu2@gmail.com>
2025-01-19 12:08:30 +0000

    add error message for invalid multiprocess

--- src/pyper/_core/task.py
+++ src/pyper/_core/task.py
@@ -48,9 +48,19 @@ class Task:
             or inspect.iscoroutinefunction(func.__call__) \
             or inspect.isasyncgenfunction(func.__call__)
         
-        if self.is_async and multiprocess:
-            raise ValueError("multiprocess cannot be True for an async task")
-        
+        if multiprocess:
+            # Asynchronous functions cannot be multiprocessed
+            if self.is_async:
+                raise ValueError("multiprocess cannot be True for an async task")
+            
+            # The function needs to be globally accessible to be multiprocessed
+            # This excludes objects like lambdas and closures
+            # We capture these cases to throw a clear error message
+            module = inspect.getmodule(func)
+            if module is None or getattr(module, func.__name__, None) is not func:
+                raise RuntimeError(f"{func} cannot be multiprocessed because it is not globally accessible"
+                                   f" -- it must be a globally defined object accessible by the name {func.__name__}")
+            
         self.func = func if bind is None else functools.partial(func, *bind[0], **bind[1])
         self.branch = branch
         self.join = join
